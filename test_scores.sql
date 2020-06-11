753d3d5d4ccdf4a5d8a5b830579d02d0fe6a89ad
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-06-11 15:36:25 -0600

    adding ACT Match Ranges

--- test_scores.sql
+++ test_scores.sql
@@ -1,36 +1,61 @@
-select spriden_id dixieid,
-       spriden_first_name first_name,
-       spriden_last_name last_name
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 2)) act_math,
-       (select sortest_test_score
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 3)) act_math_old,
-       (select sortest_test_score
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 2)) as act_math_max,
-       (select sortest_test_score
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 4)) act_reading
-from saturn.spriden@proddb
-where spriden_pidm in ('88835',
-                       '105747',
-                       '153040',
-                       '153169',
-                       '157510',
-                       '179806',
-                       '191154',
-                       '192738',
-                       '194309',
-                       '205619');
+-- ACT Math
+with cte_act1 as (select spriden_pidm,
+                         (select sortest_test_score
+                          from saturn.sortest@proddb
+                          where sortest.rowid = f_get_sortest_rowid@proddb(spriden_pidm, 'ADMSTEST', 2)) act_math,
+                         (select sortest_test_score
+                          from saturn.sortest@proddb
+                          where sortest.rowid = f_get_sortest_rowid@proddb(spriden_pidm, 'ADMSTEST', 3)) act_math_new
+                  from saturn.spriden@proddb
+                  where spriden_change_ind is null
+                    and spriden_entity_ind = 'P')
+select a.dsc_pidm,
+       b.act_math,
+       b.act_math_new,
+       greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math)) as act_max_math_score
+from students03 a,
+     cte_act1 b
+where a.dsc_pidm = spriden_pidm
+  and a.dsc_term_code = '201943'
+  and a.s_level <> 'GG'
+  and a.s_deg_intent <> '0'
+  and a.s_entry_action in ('FF', 'FH')
+  and greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math)) is not null;
 
-
-/*
-        (select sortest_test_score
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 5)) "Act_Composite",
-       (select sortest_test_score
-        from saturn.sortest
-        where sortest.rowid = F_GET_SORTEST_ROWID(spriden_pidm, 'ADMSTEST', 1)) act_engl,
-       (select sortest_test_score
- */
+-- ACT Math Range
+with cte_act1 as (select spriden_pidm,
+                         (select sortest_test_score
+                          from saturn.sortest@proddb
+                          where sortest.rowid = f_get_sortest_rowid@proddb(spriden_pidm, 'ADMSTEST', 2)) act_math,
+                         (select sortest_test_score
+                          from saturn.sortest@proddb
+                          where sortest.rowid = f_get_sortest_rowid@proddb(spriden_pidm, 'ADMSTEST', 3)) act_math_new
+                  from saturn.spriden@proddb
+                  where spriden_change_ind is null
+                    and spriden_entity_ind = 'P')
+select count(distinct a.dsc_pidm),
+       case
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '30' and '36' then '30-36'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '24' and '29' then '24-29'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '18' and '23' then '18-23'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '12' and '17' then '12-17'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '6' and '11' then '6-11'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  < '6' then '6 and below'
+        end as act_math_range
+from students03 a,
+     cte_act1 b
+where a.dsc_pidm = spriden_pidm
+  and a.dsc_term_code = '201943'
+  and a.s_level <> 'GG'
+  and a.s_deg_intent <> '0'
+  and a.s_entry_action in ('FF', 'FH')
+  and greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math)) is not null
+  group by
+         case
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '30' and '36' then '30-36'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '24' and '29' then '24-29'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '18' and '23' then '18-23'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '12' and '17' then '12-17'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  between '6' and '11' then '6-11'
+           when greatest(coalesce(act_math, act_math_new), coalesce(act_math_new, act_math))  < '6' then '6 and below'
+        end;
